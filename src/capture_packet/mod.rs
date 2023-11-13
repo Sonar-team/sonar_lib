63366bda88ef36488231db40c9f4dab38ce8d8f7
akmot9 <avicocyprien@yahoo.com>
2023-11-13 16:20:29 +0100

    add all in list inter

--- src/capture_packet/mod.rs
+++ src/capture_packet/mod.rs
@@ -111,11 +111,13 @@ pub fn get_interfaces() -> Vec<String> {
     let interfaces = datalink::interfaces();
     println!("Fetching network interfaces");
 
-    let names: Vec<String> = interfaces.iter().map(|iface| {
+    let mut names: Vec<String> = interfaces.iter().map(|iface| {
         let name = iface.name.clone();
         println!("Found interface: {}", name);
         name
     }).collect();
+    let all = String::from("all");
+    names.push(all);
 
     names
 }
